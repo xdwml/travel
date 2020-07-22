2c6b7561c569821f21a573e34b649fc4d5e2558a
wml <18270856781@163.com>
2020-07-22 08:04:17 +0800

    Signed-off-by: wml <18270856781@163.com>

--- travel/src/main/java/cn/itcast/travel/web/servlet/BaseServlet.java
+++ travel/src/main/java/cn/itcast/travel/web/servlet/BaseServlet.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,72 +11,60 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+
 public class BaseServlet extends HttpServlet {
+
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //System.out.println("BaseServlet中的service方法被执行了。。。");
-        //完成方法的分发
+        //System.out.println("baseServlet的service方法被执行了...");
+
+        //完成方法分发
         //1.获取请求路径
-        String uri=req.getRequestURI();//   /travel/user/add
-       // System.out.println("请求uri:"+uri); //  /travel/user/add
+        String uri = req.getRequestURI(); //   /travel/user/add
+        System.out.println("请求uri:"+uri);//  /travel/user/add
         //2.获取方法名称
-        String methodName=uri.substring(uri.lastIndexOf('/')+1);
-       // System.out.println("方法名称："+methodName);
+        String methodName = uri.substring(uri.lastIndexOf('/') + 1);
+        System.out.println("方法名称："+methodName);
         //3.获取方法对象Method
-        //this为谁调用我，我代表谁
-        System.out.println(this);//UserServlet的对象
+        //谁调用我？我代表谁
+        System.out.println(this);//UserServlet的对象cn.itcast.travel.web.servlet.UserServlet@4903d97e
         try {
-            //getDeclaredMethod忽略访问权限修饰符，获取方法
-            Method method=this.getClass().getMethod(methodName,HttpServletRequest.class,HttpServletResponse.class);
+            //获取方法
+            Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
             //4.执行方法
-            //method.setAccessible(true); //暴力反射
+            //暴力反射
+            //method.setAccessible(true);
             method.invoke(this,req,resp);
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
-        }catch (IllegalAccessException e) {
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
+
+
     }
 
     /**
-<<<<<<< HEAD
-     * 直接将传入的对象序列化json,并且写回客户端
-     * @param obj
-     * @param response
-     * @throws IOException
-     */
-    public void writeValue(Object obj,HttpServletResponse response) throws IOException {
-        ObjectMapper mapper=new ObjectMapper();
-        response.setContentType("application/json;charset=utf-8");//设置content-type
-=======
      * 直接将传入的对象序列化为json，并且写回客户端
      * @param obj
      */
     public void writeValue(Object obj,HttpServletResponse response) throws IOException {
-        ObjectMapper mapper=new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper();
         response.setContentType("application/json;charset=utf-8");
->>>>>>> b81a403f92b47fa101380431d7a7d0eae36cbb2f
         mapper.writeValue(response.getOutputStream(),obj);
     }
 
     /**
-<<<<<<< HEAD
-     * 将传入的对象序列化为json，返回字符串
+     * 将传入的对象序列化为json，返回
      * @param obj
      * @return
      */
-    public String  writeValueAsString(Object obj) throws JsonProcessingException {
-        ObjectMapper mapper=new ObjectMapper();
+    public String writeValueAsString(Object obj) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
         return mapper.writeValueAsString(obj);
-=======
-     * 将传入的对象序列化为json，返回
-     * @param obj
-     */
-    public void writeValueAsString(Object obj) throws JsonProcessingException {
-        ObjectMapper mapper=new ObjectMapper();
-        mapper.writeValueAsString(obj);
->>>>>>> b81a403f92b47fa101380431d7a7d0eae36cbb2f
     }
+
 }
